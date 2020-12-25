84916b83b6dbed48a8e0b3b09904229f6d098797
Aashay1098 <70257919+Aashay1098@users.noreply.github.com>
2020-12-25 21:18:34 +0530

    Convert The Layout to functional component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,41 +1,36 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import classes from "./Layout.css";
 import Aux from "../Container";
 import ToolBar from "../../components/Navigation/ToolBar/ToolBar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 import { connect } from "react-redux";
 
-class Layout extends Component {
-	state = {
-		showSideDrawer: false,
+const Layout = (props) => {
+	const [showSideDrawer, setShowSideDrawer] = useState(false);
+	const sideDrawerCloseHandler = () => {
+		setShowSideDrawer(false);
 	};
-	sideDrawerCloseHandler = () => {
-		this.setState({ showSideDrawer: false });
+	const sideDrawerToggleHandler = () => {
+		setShowSideDrawer(!showSideDrawer);
 	};
-	sideDrawerToggleHandler = () => {
-		this.setState({ showSideDrawer: !this.state.showSideDrawer });
-	};
-	render() {
-		return (
-			<Aux>
-				<div>
-					<ToolBar
-						isAuth={this.props.isAuthenticated}
-						toggleClicked={this.sideDrawerToggleHandler}
-					/>
-					<SideDrawer
-						isAuth={this.props.isAuthenticated}
-						isOpen={this.state.showSideDrawer}
-						closed={this.sideDrawerCloseHandler}
-					/>
-				</div>
-				<main className={classes.Content}>
-					{this.props.children}
-				</main>
-			</Aux>
-		);
-	}
-}
+
+	return (
+		<Aux>
+			<div>
+				<ToolBar
+					isAuth={props.isAuthenticated}
+					toggleClicked={sideDrawerToggleHandler}
+				/>
+				<SideDrawer
+					isAuth={props.isAuthenticated}
+					isOpen={showSideDrawer}
+					closed={sideDrawerCloseHandler}
+				/>
+			</div>
+			<main className={classes.Content}>{props.children}</main>
+		</Aux>
+	);
+};
 
 const mapStateToProps = (state) => {
 	return {
